56b6e54611209e6938325d791e4c6420bc4715e1
Sammyferreira1 <sammyfe2021@gmail.com>
2023-04-25 14:29:59 -0300

    att infors tec

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -13,9 +13,9 @@ import {
     { id: "html", name: "HTML5", icon: <DiHtml5 />, texto: 'Experiência e conhecimento avançado em HTML' },
     { id: "css", name: "CSS3", icon: <DiCss3 />, texto: 'Experiência e conhecimento avançado em CSS' },
     { id: "js", name: "JavaScript", icon: <DiJsBadge />, texto: 'Experiência e conhecimento avançado em JavaScript' },
+    { id: "sass", name: "Sass", icon: <DiSass />, texto: 'Conhecimento em Sass' },
     { id: "node", name: "Node.js", icon: <DiNodejsSmall />, texto: 'Estudando NodeJs' },
     { id: "react", name: "React", icon: <DiReact />, texto: 'Estudando ReactJs' },
-    { id: "sass", name: "Sass", icon: <DiSass />, texto: 'Estudando Sass' }
   ];
   
   const TechnologiesContainer = () => {
